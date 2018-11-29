e7be0316492bc222c6a78c3bde9d65ca2d91faed
Ryan Y <yakstisrf@innovasi.com>
2018-11-29 13:45:38 -0500

    Clean up startup code
    Clean up the startup code so we can maintain it better.
    
    Signed-off-by: Ryan Y <yakstisrf@innovasi.com>

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.WsFederation;
 using Microsoft.AspNetCore.Builder;
@@ -8,9 +9,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using wsfed_issue.Providers;
 
 namespace wsfed_issue {
     public class Startup {
@@ -27,97 +30,112 @@ namespace wsfed_issue {
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDataProtection();
-            services.Configure<ForwardedHeadersOptions>(options => {
-                var addresses = Dns.GetHostAddresses("web");
-                foreach (var address in addresses)
-                    options.KnownProxies.Add(address);
-            });
+            services.Configure<ForwardedHeadersOptions>(
+                ConfigureForwardedHeaders);
 
-            services.AddAuthentication(
-                    auth => {
-                        auth.DefaultScheme = CookieAuthenticationDefaults
-                            .AuthenticationScheme;
-                        auth.DefaultChallengeScheme = WsFederationDefaults
-                            .AuthenticationScheme;
-
-                    })
-                .AddCookie()
-                .AddWsFederation(
-                    WsFederationDefaults.AuthenticationScheme,
-                    "Org Auth",
-                    options => {
-                        // MetadataAddress represents the Active Directory instance used to authenticate users.
-                        options.MetadataAddress =
-                            Configuration["WsAuthMetadataAddress"];
-                        // Wtrealm is the app's identifier in the Active Directory instance.
-                        // For ADFS, use the relying party's identifier, its WS-Federation Passive protocol URL:
-                        options.Wtrealm = Configuration["WsAuthrealm"];
-                    });
+            AddAuthenticationTo(services);
 
             // Simple example with dependency injection for a data provider.
-            services
-                .AddSingleton<Providers.IWeatherProvider,
-                    Providers.WeatherProviderFake>();
+            services.AddSingleton<IWeatherProvider, WeatherProviderFake>();
+        }
+
+        private void ConfigureForwardedHeaders(
+            ForwardedHeadersOptions options) {
+            var addresses = Dns.GetHostAddresses("web");
+
+            foreach (var address in addresses)
+                options.KnownProxies.Add(address);
+        }
+
+        private void AddAuthenticationTo(IServiceCollection services) {
+            var builder = services.AddAuthentication(ConfigureAuthentication);
+            builder.AddCookie();
+            builder.AddWsFederation(
+                WsFederationDefaults.AuthenticationScheme,
+                "Org Auth",
+                ConfigureWsFederation);
+        }
+
+        private void ConfigureAuthentication(AuthenticationOptions auth) {
+            auth.DefaultScheme =
+                CookieAuthenticationDefaults.AuthenticationScheme;
+            auth.DefaultChallengeScheme =
+                WsFederationDefaults.AuthenticationScheme;
+        }
+
+        private void ConfigureWsFederation(WsFederationOptions options) {
+            // MetadataAddress represents the Active Directory instance used to authenticate users.
+            options.MetadataAddress = Configuration["WsAuthMetadataAddress"];
+
+            // Wtrealm is the app's identifier in the Active Directory instance.
+            // For ADFS, use the relying party's identifier, its WS-Federation Passive protocol URL:
+            options.Wtrealm = Configuration["WsAuthrealm"];
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(
             IApplicationBuilder app,
             IHostingEnvironment env) {
-            if (env.IsDevelopment()) {
-                app.UseDeveloperExceptionPage();
-
-                // Webpack initialization with hot-reload.
-                app.UseWebpackDevMiddleware(
-                    new WebpackDevMiddlewareOptions {
-                        HotModuleReplacement = true,
-                    });
-            } else {
+            if (env.IsDevelopment())
+                ConfigureDevelopmentFor(app);
+            else
                 app.UseExceptionHandler("/Home/Error");
-            }
 
             app.UseStaticFiles();
-            app.UseForwardedHeaders(new ForwardedHeadersOptions
-            {
-                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-            });
+            var forwardedOptions = new ForwardedHeadersOptions {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor
+                    | ForwardedHeaders.XForwardedProto
+            };
+            app.UseForwardedHeaders(forwardedOptions);
             app.UseAuthentication();
 
-            app.UseMvc(
-                routes => {
-                    routes.MapRoute(
-                        name: "default",
-                        template: "{controller=Home}/{action=Index}/{id?}");
-
-                    routes.MapSpaFallbackRoute(
-                        name: "spa-fallback",
-                        defaults: new {controller = "Home", action = "Index"});
-                });
-
+            app.UseMvc(ConfigureRoutes);
             app.Use(Middleware);
         }
 
+        private void ConfigureDevelopmentFor(IApplicationBuilder app) {
+            var webpackOptions = new WebpackDevMiddlewareOptions {
+                HotModuleReplacement = true,
+            };
+
+            // Webpack initialization with hot-reload.
+            app.UseWebpackDevMiddleware(webpackOptions);
+            app.UseDeveloperExceptionPage();
+        }
+
+        private void ConfigureRoutes(IRouteBuilder routes) {
+            routes.MapRoute(
+                "default",
+                "{controller=Home}/{action=Index}/{id?}");
+            routes.MapSpaFallbackRoute(
+                "spa-fallback",
+                new {controller = "Home", action = "Index"});
+        }
+
         private async Task Middleware(HttpContext context, Func<Task> next) {
-            context.Response.ContentType = "text/plain";
+            var newline = Environment.NewLine;
+            var request = context.Request;
+            var response = context.Response;
+            response.ContentType = "text/plain";
 
             // Request method, scheme, and path
-            await context.Response.WriteAsync(
-                $"Request Method: {context.Request.Method}{Environment.NewLine}");
-            await context.Response.WriteAsync(
-                $"Request Scheme: {context.Request.Scheme}{Environment.NewLine}");
-            await context.Response.WriteAsync(
-                $"Request Path: {context.Request.Path}{Environment.NewLine}");
+            await response.WriteAsync(
+                $"Request Method: {request.Method}{newline}");
+            await response.WriteAsync(
+                $"Request Scheme: {request.Scheme}{newline}");
+            await response.WriteAsync($"Request Path: {request.Path}{newline}");
 
             // Headers
-            await context.Response.WriteAsync($"Request Headers:{Environment.NewLine}");
+            await response.WriteAsync($"Request Headers:{newline}");
 
-            foreach (var header in context.Request.Headers)
-                await context.Response.WriteAsync($"{header.Key}: " + $"{header.Value}{Environment.NewLine}");
+            foreach (var header in request.Headers)
+                await response.WriteAsync(
+                    $"{header.Key}: " + $"{header.Value}{newline}");
 
-            await context.Response.WriteAsync(Environment.NewLine);
+            await response.WriteAsync(newline);
 
             // Connection: RemoteIp
-            await context.Response.WriteAsync(
+            await response.WriteAsync(
                 $"Request RemoteIp: {context.Connection.RemoteIpAddress}");
             await next();
         }
